9e99c269be64f82557e87ccd89e0683eb5895b9d
JZwlth <wkjazy87@gmail.com>
2024-11-14 19:41:28 -0800

    update

--- main.cpp
+++ main.cpp
@@ -8,7 +8,24 @@ using namespace std;
 
 const int INITIAL_SIZE = 2;
 
-int main{
+int main() {
+    // Seed the random number generator
+    srand(static_cast<unsigned>(time(0)));
     
+    deque<Car> carQueue;
+
+    for (int i = 0; i < INITIAL_SIZE; i++) {
+        carQueue.push_back(Car());
+    }
+
+    // Display the initial queue
+    cout << "Initial queue:\n";
+    for (auto &car : carQueue) {
+        car.print();
+    }
+    cout << endl;
+
+    int time = 1; 
+
     return 0;
 }
