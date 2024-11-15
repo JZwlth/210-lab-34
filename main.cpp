d41bdb786999c81e9ee425c6692273374d5ff59e
JZwlth <wkjazy87@gmail.com>
2024-11-14 20:01:46 -0800

    update

--- main.cpp
+++ main.cpp
@@ -27,5 +27,34 @@ int main() {
 
     int time = 1; 
 
+    while (!carQueue.empty()) {
+        int operation = rand() % 100 + 1;  // Random number between 1 and 100
+        
+        if (operation <= 55) {
+            // 55% chance for the car at the head of the queue to pay and leave
+            cout << "Time: " << time << " Operation: Car paid: ";
+            carQueue.front().print();
+            carQueue.pop_front();
+        } else {
+            // 45% chance for a new car to join the line
+            Car newCar;
+            cout << "Time: " << time << " Operation: Joined lane: ";
+            newCar.print();
+            carQueue.push_back(newCar);
+        }
+
+        cout << "Queue:\n";
+        if (carQueue.empty()) {
+            cout << "    Empty\n";
+        } else {
+            for (auto &car : carQueue) {
+                car.print();
+            }
+        }
+        cout << endl;
+
+        time++; // Increment time for each cycle
+    }
+
     return 0;
 }
