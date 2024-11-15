0acf8edbcdf85f470bf3fc0219bf952d84829348
JZwlth <wkjazy87@gmail.com>
2024-11-14 19:30:13 -0800

    update

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include "Car.h"
+
+using namespace std;
+
+const int INITIAL_SIZE = 2;
+
+int main{
+    
+    return 0;
+}
